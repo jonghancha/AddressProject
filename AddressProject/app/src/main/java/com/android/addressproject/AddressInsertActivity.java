36294e2ea5e15c6a989f6808b103911a82356f45
차종한 <jonghancha@gmail.com>
2020-12-23 18:16:41 +0900

    CRU Network + NetworkTask 생성 각 화면 별 activity + xml 생성.

--- AddressProject/app/src/main/java/com/android/addressproject/AddressInsertActivity.java
+++ AddressProject/app/src/main/java/com/android/addressproject/AddressInsertActivity.java
@@ -4,16 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-    // 메인화면입니다. 로그인 성공시 뜨는 화면입니다.
+public class AddressInsertActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_address_insert);
     }
-
-
-
-
 }
