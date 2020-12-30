b1dcf746fd03b4a6bdd5532b98e2291856d52411
차종한 <jonghancha@gmail.com>
2020-12-30 12:02:01 +0900

    20.12.30 종한
    1. okhttp3 모듈 추가
    2. ImageUpload RnD 내용 InsertActivity에 전부 추가
    3. 로그인 성공시 MainActivity onCreate 처음에 파일관련 사용자 권한 물어보기 추가
    4. activity_insert.xml 연락처 등록 형식에 맞게 수정

--- AddressProject/app/src/main/java/com/android/addressproject/Activity/InsertActivity.java
+++ AddressProject/app/src/main/java/com/android/addressproject/Activity/InsertActivity.java
@@ -1,17 +1,27 @@
 package com.android.addressproject.Activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,7 +29,35 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import com.android.addressproject.R;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class InsertActivity extends AppCompatActivity {
+
+    // 20.12.30 종한 추가
+    final  static String TAG = "InsertActivity";
+
+    ImageView ivInsertImage = null;
+    Button btnInsert = null;
+    Button btnInsertCancel = null;
+    private final int REQ_CODE_SELECT_IMAGE = 100;
+    private String img_path = new String();
+    private Bitmap image_bitmap_copy = null;
+    private Bitmap image_bitmap = null;
+    private String imageName = null;
+    private String f_ext = null;
+    File tempSelectFile;
 
     // 20.12.29 세미 추가 ------------------------------
 
@@ -57,6 +95,39 @@ public class InsertActivity extends AppCompatActivity {
 
         // 끝 --------------------------------------------
 
+        // 20.12.30 종한 추가
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .permitDiskReads()
+                .permitDiskWrites()
+                .permitNetwork().build());
+
+        ivInsertImage =  findViewById(R.id.insert_image);
+        //이미지를 띄울 위젯
+        ivInsertImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, REQ_CODE_SELECT_IMAGE);
+            }
+        });
+
+        btnInsert = findViewById(R.id.btn_insert);
+        //이미지 전송 버튼
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // 네트워크 연결 (Thread = asynctask)
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        doMultiPartRequest();
+                    }
+                }).start();
+            }
+        });
 
     }//--onCreate 끝
 
@@ -93,13 +164,14 @@ public class InsertActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == SETTINGS_CODE){
-            this.recreate();
-        }
-    }
+    // onActivityResult 종한.이 가지고있던거 사용 함
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == SETTINGS_CODE){
+//            this.recreate();
+//        }
+//    }
 
     ///테마
     private void setThemeOfApp(){
@@ -134,5 +206,106 @@ public class InsertActivity extends AppCompatActivity {
         return super.dispatchTouchEvent(ev);
     }
 
+    // 20.12.30 종한 추가
+    // 갤러리에서 선택한 사진 경로 받아오기
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Toast.makeText(getBaseContext(), "resultCode : " + data, Toast.LENGTH_SHORT).show();
+
+        if (requestCode == REQ_CODE_SELECT_IMAGE) {
+            if (resultCode == Activity.RESULT_OK) {
+                try {
+                    img_path = getImagePathToUri(data.getData()); //이미지의 URI를 얻어 경로값으로 반환.
+                    Toast.makeText(getBaseContext(), "img_path : " + img_path, Toast.LENGTH_SHORT).show();
+                    Log.v("test", String.valueOf(data.getData()));
+                    //이미지를 비트맵형식으로 반환
+                    image_bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+
+                    //사용자 단말기의 width , height 값 반환
+                    int reWidth = (int) (getWindowManager().getDefaultDisplay().getWidth());
+                    int reHeight = (int) (getWindowManager().getDefaultDisplay().getHeight());
+
+                    //image_bitmap 으로 받아온 이미지의 사이즈를 임의적으로 조절함. width: 400 , height: 300
+                    image_bitmap_copy = Bitmap.createScaledBitmap(image_bitmap, 400, 300, true);
+                    ImageView image = findViewById(R.id.insert_image);  //이미지를 띄울 위젯 ID값
+                    image.setImageBitmap(image_bitmap_copy);
+
+                    // 파일 이름 및 경로 바꾸기(임시 저장)
+                    String date = new SimpleDateFormat("yyyyMMddHmsS").format(new Date());
+                    imageName = date+"."+f_ext;
+                    tempSelectFile = new File("/data/data/com.androidlec.imageupload/", imageName);
+                    OutputStream out = new FileOutputStream(tempSelectFile);
+                    image_bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+
+                    // 임시 파일 경로로 위의 img_path 재정의
+                    img_path = "/data/data/com.androidlec.imageupload/"+imageName;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }//end of onActivityResult()
+
+    public String getImagePathToUri(Uri data) {
+        //사용자가 선택한 이미지의 정보를 받아옴
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery(data, proj, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+
+        //이미지의 경로 값
+        String imgPath = cursor.getString(column_index);
+        Log.d("test", imgPath);
+
+        //이미지의 이름 값
+        String imgName = imgPath.substring(imgPath.lastIndexOf("/") + 1);
+
+        // 확장자 명 저장
+        f_ext = imgPath.substring(imgPath.length()-3, imgPath.length());
+        Toast.makeText(InsertActivity.this, "이미지 이름 : " + imgName, Toast.LENGTH_SHORT).show();
+        this.imageName = imgName;
+
+        return imgPath;
+    }//end of getImagePathToUri()
+
+
+
+    //파일 변환
+    private void doMultiPartRequest() {
+
+        File f = new File(img_path);
+
+        DoActualRequest(f);
+    }
+
+    //서버 보내기
+    private void DoActualRequest(File file) {
+        OkHttpClient client = new OkHttpClient();
+        Log.v(TAG,"Called actual request");
+        String url = "http://localhost:8080/test/multipartRequest.jsp";
+
+        RequestBody body = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("image", file.getName(),
+                        RequestBody.create(MediaType.parse("image/jpeg"), file))
+                .build();
+        Log.v(TAG,"Request body generated");
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            Log.v(TAG,"Request successful");
+            Log.v(TAG,response.body().string());
+        } catch (IOException e) {
+            Log.v(TAG,"Exception occured :" + e.toString());
+            e.printStackTrace();
+        }
+    }
 
 }//-- 전체 끝
