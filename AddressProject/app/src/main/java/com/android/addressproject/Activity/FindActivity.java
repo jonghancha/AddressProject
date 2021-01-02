4a4c59bc58ff439b37b1226737f24788ea6fd371
imkanghooo <imkanghoo@icloud.com>
2021-01-03 08:02:11 +0900

    아이디찾기/비번찾기에 화면 클릭시 키보드 숨기기 기능(세미가 한것) 추가

--- AddressProject/app/src/main/java/com/android/addressproject/Activity/FindActivity.java
+++ AddressProject/app/src/main/java/com/android/addressproject/Activity/FindActivity.java
@@ -2,11 +2,14 @@ package com.android.addressproject.Activity;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -291,5 +294,22 @@ public class FindActivity extends AppCompatActivity {
     }
 
 
+    //editText 외의 화면 클릭시 키보드 숨기기
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        View focusView = getCurrentFocus();
+        if (focusView != null) {
+            Rect rect = new Rect();
+            focusView.getGlobalVisibleRect(rect);
+            int x = (int) ev.getX(), y = (int) ev.getY();
+            if (!rect.contains(x, y)) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                if (imm != null)
+                    imm.hideSoftInputFromWindow(focusView.getWindowToken(), 0);
+                focusView.clearFocus();
+            }
+        }
+        return super.dispatchTouchEvent(ev);
+    }
 
 }//---
